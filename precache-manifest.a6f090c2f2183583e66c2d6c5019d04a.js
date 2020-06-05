ab48c755f5b0d05ad70c63d2e0c0bb075fdcd2ac
ricardohung <61123892+ricardohung@users.noreply.github.com>
2020-06-05 15:10:00 -0500

    Updates

---
+++ precache-manifest.a6f090c2f2183583e66c2d6c5019d04a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d943f665cd424565a63566bc5604895",
+    "url": "/REACTCRUD.github.io/index.html"
+  },
+  {
+    "revision": "b7cadc290579001b1981",
+    "url": "/REACTCRUD.github.io/static/css/main.9a508654.chunk.css"
+  },
+  {
+    "revision": "968090486c4f1ddc090b",
+    "url": "/REACTCRUD.github.io/static/js/2.da4adfda.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/REACTCRUD.github.io/static/js/2.da4adfda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cadc290579001b1981",
+    "url": "/REACTCRUD.github.io/static/js/main.f2a54de7.chunk.js"
+  },
+  {
+    "revision": "e1e23be1bf5e45b82e95",
+    "url": "/REACTCRUD.github.io/static/js/runtime-main.788d2329.js"
+  }
+]);
