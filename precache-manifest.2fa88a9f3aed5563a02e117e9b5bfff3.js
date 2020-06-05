08425737a062f97bcdb4cb2a4df296804c5418db
ricardohung <61123892+ricardohung@users.noreply.github.com>
2020-06-05 14:53:22 -0500

    primera carga react crud hook

---
+++ precache-manifest.2fa88a9f3aed5563a02e117e9b5bfff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabd72db79e9946f57c9c177de0a0116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7cadc290579001b1981",
+    "url": "/static/css/main.9a508654.chunk.css"
+  },
+  {
+    "revision": "968090486c4f1ddc090b",
+    "url": "/static/js/2.da4adfda.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.da4adfda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cadc290579001b1981",
+    "url": "/static/js/main.f2a54de7.chunk.js"
+  },
+  {
+    "revision": "6a06dd8c58e6064993b5",
+    "url": "/static/js/runtime-main.83421ab0.js"
+  }
+]);
